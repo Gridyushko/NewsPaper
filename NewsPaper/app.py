0e154a99819d5a00ae836f0afb087c9f995e09e9
Gridyushko <109225484+Gridyushko@users.noreply.github.com>
2022-11-20 18:17:49 +0300

    Add files via upload

---
+++ NewsPaper/app.py
@@ -0,0 +1,8 @@
+import redis
+
+
+red = redis.Redis(
+    host='redis-18655.c44.us-east-1-2.ec2.cloud.redislabs.com',
+    port=18655,
+    password='JurZOAmFCMn2u5nVebyCXCu0PvqxxKOc'
+)
---
+++ news/views.py
@@ -0,0 +1,152 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Category
+from datetime import datetime
+from django.http import HttpResponseRedirect, HttpResponse
+from .filters import PostFilter
+from .forms import PostForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from .tasks import hello
+
+class IndexView(View):
+    def get(self, request):
+        hello.delay()
+        return HttpResponse('Hello!')
+
+
+
+
+def multiply(request):
+   number = request.GET.get('number')
+   multiplier = request.GET.get('multiplier')
+
+   try:
+       result = int(number) * int(multiplier)
+       html = f"<html><body>{number}*{multiplier}={result}</body></html>"
+   except (ValueError, TypeError):
+       html = f"<html><body>Invalid input.</body></html>"
+
+   return HttpResponse(html)
+
+
+
+class PostsList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = 'title'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'flatpages/news.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'posts'
+    paginate_by = 10  # вот так мы можем указать количество записей на странице
+
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = PostFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = None
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Post
+    # Используем другой шаблон — product.html
+    template_name = 'flatpages/news_id.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = None
+        return context
+
+
+
+
+class PostCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    permission_required = ('news.add_post')
+    raise_exception = True
+    # Указываем нашу разработанную форму
+    form_class = PostForm
+    # модель
+    model = Post
+    # и новый шаблон, в котором используется форма.
+    template_name = 'flatpages/news_id.html'
+
+    def create_post(request):
+        form = PostForm()
+
+        if request.method == 'POST':
+            form = PostForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect('/news/')
+
+        return render(request, 'news_edit.html', {'form': form})
+
+
+
+class PostUpdate(UpdateView, PermissionRequiredMixin):
+    permission_required = ('news.change_post')
+    form_class = PostForm
+    model = Post
+    template_name = 'flatpages/news_id.html'
+
+
+class PostDelete(DeleteView, PermissionRequiredMixin):
+    permission_required = ('news.delete_post')
+    model = Post
+    template_name = 'flatpages/news_delete.html'
+    success_url = reverse_lazy('post_list')
+
+class CategoryList(ListView):
+    model = Post
+    template_name = 'flatpages/category_list.html'
+    context_object_name = 'category_news_list'
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category,id=self.kwargs['pk'])
+        queryset = Post.objects.filter(category=self.category).order_by('date')
+        return queryset
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_subscriber'] = self.request.user not in self.category.subscribers.all()
+        context['category'] = self.category
+        return context
+
+
+def subscribe(request, pk):
+     user = request.user
+     category = Category.objects.get(id=pk)
+     category.subscribers.add(user)
+
+     message = 'Вы успешно подписались!'
+     return render(request, 'news/subscribe.html', {'category': category, 'massage': message})
+
